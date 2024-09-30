af2025ac9f800205ffe6a35abdbffdc1f6c9be54
MarcelAndreLeRoux <u22598805@tuks.co.za>
2024-09-30 17:48:45 +0200

    Basic Setup of the world
    I added some custom crosshairs that change when you click.
    I changed the background sprites.
    I made a working audio controller.
    Fixed the Sprites on the Axolotl.

---
+++ scenes/Shared/audio_controler.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Play music or sound effect by name
+func play_audio(name: String, volume: float = 1.0):
+	var audio_player = get_node(name)
+	if audio_player and audio_player is AudioStreamPlayer2D:
+		audio_player.volume_db = volume
+		audio_player.play()
+	else:
+		print("Audio node not found or incorrect type:", name)
+
+# Stop audio by name
+func stop_audio(name: String):
+	var audio_player = get_node(name)
+	if audio_player and audio_player is AudioStreamPlayer2D:
+		audio_player.stop()
+	else:
+		print("Audio node not found or incorrect type:", name)
